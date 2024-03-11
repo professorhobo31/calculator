4b011d932ca2bb512a2e8c31e1440b32696570ec
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-03-11 17:54:20 -0300

    Add initial style and scripts for basic operations

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let firstInput = 0;
+let secondInput = 0;
+let operator = 'empty';
+
+function sum(a, b) {
+    return (a + b);
+};
+
+function subtract(a, b) {
+    return (a - b);
+};
+
+function multiply(a, b) {
+    return (a * b);
+};
+
+function divide(a, b) {
+    return (a / b)
+};
