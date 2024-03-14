5df5b5c2a74025280e2bdb28ecebb608078267a8
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-03-14 18:15:52 -0300

    Add operate function

--- scripts.js
+++ scripts.js
@@ -38,6 +38,29 @@ const clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', clearButtonPressed);
 
 /**
+ * takes an operator and 2 numbers and then calls one of the above functions on the numbers
+ * @param {*} number1 first input number
+ * @param {*} number2 second input number
+ * @param {*} operator introduced as a string (+-/*) 
+ * @returns 
+ */
+function operate(number1, number2, operator) {
+    if (operator == '+') {
+        return sum(number1, number2);
+    }
+    else if (operator == '-') {
+        return subtract(number1, number2);
+    }
+    else if (operator == '*') {
+        return multiply(number1, number2);        
+    }
+    else if (operator == '/') {
+        return divide(number1, number2);        
+    }
+}
+operate(2,5,'-')
+
+/**
  * Upon pressing of the "+" button, stores the current upper display button and waits for the second input.
  */
 function sumButtonPressed() {
