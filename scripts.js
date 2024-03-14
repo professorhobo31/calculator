1deba9d57bafc9dcb70adc5214cc3bed2b79a561
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-03-14 18:03:06 -0300

    Add clear button functionality and sum skeleton

--- scripts.js
+++ scripts.js
@@ -28,3 +28,27 @@ numberButtons.forEach(element => {
     });
 });
 
+/**
+ * Clears the upper display zone
+ */
+function clearButtonPressed() {
+    display.textContent = undefined;
+}
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', clearButtonPressed);
+
+/**
+ * Upon pressing of the "+" button, stores the current upper display button and waits for the second input.
+ */
+function sumButtonPressed() {
+    if (display.textContent == undefined) {
+        firstInput = 0;
+    }
+    else {
+        firstInput = Number(display.innerText);
+    };
+    console.log(firstInput);
+}
+
+const sumButton = document.getElementById('+');
+sumButton.addEventListener('click', sumButtonPressed);
